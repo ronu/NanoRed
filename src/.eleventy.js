d0e8ad7ff74868f44bd0be32a768e4848e5bec58
Nic <github@nicfv.com>
2025-09-28 13:18:43 -0700

    Add log filter

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,4 +1,5 @@
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('**/*.css');
     eleventyConfig.addPassthroughCopy('**/*.ttf');
+    eleventyConfig.addFilter('log', any => console.log(any));
 }

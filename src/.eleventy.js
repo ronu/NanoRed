3edc9b280a9d69370ce2ca2c6fcaabaa7cdd5bb9
Nic <github@nicfv.com>
2025-09-28 16:57:29 -0700

    add filter to return object keys

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -2,4 +2,5 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('**/*.css');
     eleventyConfig.addPassthroughCopy('**/*.ttf');
     eleventyConfig.addFilter('log', any => console.log(any));
+    eleventyConfig.addFilter('keys', obj => Object.keys(obj));
 }

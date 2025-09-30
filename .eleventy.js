160224fd1cefced7dad74562a5322a369a1c98ba
nicfv <github@nicfv.com>
2025-09-29 18:29:03 -0700

    I guess its src/media now

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 export default function (eleventyConfig) {
     eleventyConfig.setInputDirectory('src');
-    eleventyConfig.addPassthroughCopy('media');
+    eleventyConfig.addPassthroughCopy('src/media');
     eleventyConfig.addFilter('log', any => console.log(any));
     eleventyConfig.addFilter('keys', obj => Object.keys(obj));
 }

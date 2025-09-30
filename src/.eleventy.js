0203164f2cb3ddee8d72d15b57f52ae8353806b4
nicfv <github@nicfv.com>
2025-09-29 17:01:45 -0700

    copy everything in media/

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,6 +1,5 @@
 export default function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('**/*.css');
-    eleventyConfig.addPassthroughCopy('**/*.ttf');
+    eleventyConfig.addPassthroughCopy('media');
     eleventyConfig.addFilter('log', any => console.log(any));
     eleventyConfig.addFilter('keys', obj => Object.keys(obj));
 }

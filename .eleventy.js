61b2acb7e4e5801e9bfdd1853c8a0bec64f8b1ec
nicfv <github@nicfv.com>
2025-09-29 18:27:39 -0700

    Move config files to base directoryu

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 export default function (eleventyConfig) {
+    eleventyConfig.setInputDirectory('src');
     eleventyConfig.addPassthroughCopy('media');
     eleventyConfig.addFilter('log', any => console.log(any));
     eleventyConfig.addFilter('keys', obj => Object.keys(obj));
